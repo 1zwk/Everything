a56284972cfc705ea14394cc31fdca5a25e6ee9d
S ZHANG <459617255@qq.com>
2019-12-28 16:48:49 +0800

    1

--- src/main/java/com/github/everything/core/dao/DataSourceFactory.java
+++ src/main/java/com/github/everything/core/dao/DataSourceFactory.java
@@ -1,5 +1,9 @@
 package com.github.everything.core.dao;
 
+import com.sun.xml.internal.bind.v2.util.DataSourceSource;
+
+import javax.activation.DataSource;
+
 /**
  * 创建数据源 的 单例工厂类
  */
@@ -7,13 +11,20 @@ public class DataSourceFactory {
     /**
      * 数据源（单例）
      */
-//    private static volatile DruidDataSource dataSource;
-//
-//    private DataSourceFactory(){
-//
-//    }
-//    public static DataSource dataSource(){
-//        if()
-//    }
+    private static volatile DruidDataSource dataSource;
+
+
+    private DataSourceFactory(){
+
+    }
+    public static DataSource dataSource(){
+        if(dataSouce == null){
+            synchronized (DataSourceSource.class){
+                if(dataSouce == null){
+
+                }
+            }
+        }
+    }
 
 }
